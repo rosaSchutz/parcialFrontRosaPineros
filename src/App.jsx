f5534d35f8c50340cd4ecb121ce8041570ca3673
rosaSchutz <rosschutz1997@gmail.com>
2023-10-30 18:54:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Card from "./Components/Card";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
